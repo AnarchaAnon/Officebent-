3bdd20c5c30dee85bec2c9dce65370080408b2a9
AnarchaAnon <43900091+AnarchaAnon@users.noreply.github.com>
2018-10-06 00:41:30 -0400

    Add files via upload

---
+++ officeBentFirstJava.js
@@ -0,0 +1,11 @@
+﻿//java functions to replace an image! ☆~~(>w0✿)b
+function razFunction() {
+    document.getElementById("actGrlBtn").id = "grlBtn";
+    document.getElementById("razBtn").id = "actRazBtn";
+    document.getElementById("eightyEightImg").src = "https://i.imgur.com/mLgFWcu.png"
+}
+function grlFunction() {
+    document.getElementById("actRazBtn").id = "razBtn";
+    document.getElementById("grlBtn").id = "actGrlBtn";
+    document.getElementById("eightyEightImg").src = "https://i.imgur.com/OcKHHe9.png";
+}
